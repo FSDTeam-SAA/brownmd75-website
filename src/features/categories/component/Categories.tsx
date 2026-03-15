a91a80dcbd4fa42a02b5b05cbc57334c5a13bb42
RashedulHaqueRasel1 <rashedulhaquerasel1@gmail.com>
2026-03-16 00:55:44 +0600

    feat: Add new categories page and its corresponding component

---
+++ src/features/categories/component/Categories.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Hero from "@/components/sheared/Hero";
+
+export default function Categories() {
+  return (
+    <div className="">
+      <Hero
+        image="/images/hero-2.jpg"
+        heading="Rent Smarter, Build Faster"
+        description="Choose from our professionally maintained fleet across multiple categories to keep your work moving forward."
+      />
+    </div>
+  );
+}
